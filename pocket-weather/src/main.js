dab4d1f9a27e3c5d090b25b9bade555a012bfa59
vanessa <jumpingboys@naver.com>
2021-01-12 02:48:17 +0900

    [PW] Install SCSS and reset styles.

--- pocket-weather/src/main.js
+++ pocket-weather/src/main.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 import router from './router/router.js';
 import App from './App.vue';
 
+// import style
+import '../scss/index.scss';
+
 createApp(App)
   .use(router, Vuex, axios)
   .mount('#app');
