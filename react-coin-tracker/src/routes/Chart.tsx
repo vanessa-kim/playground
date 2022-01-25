97214b37b9333a3f4796c6438998eed0aa3efbc7
vanessa <vanessakim90@nexr.com>
2022-01-25 22:00:54 +0900

    [CT] Making Coin tracker mini project using React

---
+++ react-coin-tracker/src/routes/Chart.tsx
@@ -0,0 +1,76 @@
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from '../api';
+import ApexChart from 'react-apexcharts';
+
+interface ChartProps {
+  coinId: string;
+}
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume:number;
+  market_cap: number;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const {isLoading, data} = useQuery<IHistorical[]>(['ohlcv', coinId], () => fetchCoinHistory(coinId));
+  
+  return <div>{ isLoading ? "loading chart..." : (
+    <ApexChart 
+      type="line"
+      series={[
+        {
+          name: 'price',
+          data: data?.map(price => price.close),
+        },
+      ]}
+      options={{
+        theme: {
+          mode: 'dark',
+        },
+        chart: {
+          height: 300,
+          width: 500,
+          toolbar: {
+            show: false,
+          },
+          background: 'transparent',
+        },
+        grid: {
+          show: false,
+        },
+        stroke: {
+          curve: 'smooth',
+          width: 2,
+        },
+        yaxis: {
+          show: false,
+        },
+        xaxis: {
+          labels: { show: false },
+          axisTicks: { show: false },
+          axisBorder: { show: false },
+          categories: data?.map(price => price.time_close),
+          type: 'datetime',
+        },
+        fill: { 
+          type: 'gradient', 
+          gradient: { gradientToColors: ['#1abc9c'], stops: [0, 100]},
+        },
+        colors: ['#3498db'],
+        tooltip: {
+          y: {
+            formatter: (value) => `$ ${value.toFixed(3)}`,
+          },
+        },
+      }}
+    />
+  )}</div>;
+}
+
+export default Chart;
